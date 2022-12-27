106630958d698e584cb83574682ae9a024e5dd82
Rick Spencer <rick@influxdata.com>
2022-12-27 06:03:14 -0800

    added ability to add headers to checks

--- app/checks/routes.py
+++ app/checks/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for
 from flask_user import login_required, current_user
 from app.checks import bp
 from app.models.checks import Check
+from app.models.headers import Header
 from app.extensions import db
 
 @bp.route('/')
@@ -10,16 +11,24 @@ def index():
     all_checks = Check.query.all()
     return render_template('checks/index.html', checks = all_checks)
 
-@bp.route('/<id>')
+@bp.route('/<check_id>')
 @login_required
-def details(id):
-    check = Check.query.get(id)
+def details(check_id):
+    check = Check.query.get(check_id)
     return render_template('checks/details.html', check=check)
 
-@bp.route('/<id>/headers')
+@bp.route('/<check_id>/headers', methods=["GET","POST"])
 @login_required
-def new_header(id):
-    return render_template('headers/new.html', id=id)
+def new_header(check_id):
+    if request.method == "GET":
+        return render_template('headers/new.html')
+    elif request.method == "POST":
+        header = Header(key = request.form['key'], 
+                        value = request.form['value'],
+                        check_id = check_id)
+        db.session.add(header)
+        db.session.commit()
+        return redirect(url_for('checks.details', check_id=check_id))
 
 @bp.route('/new', methods=["GET","POST"])
 @login_required
