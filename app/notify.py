0046bf00a828b08287ed368bec0def082a0c4aa2
Rick Spencer <rick@influxdata.com>
2022-12-31 13:51:15 -0800

    fixed and cleaned up notify.py

--- app/notify.py
+++ app/notify.py
@@ -2,30 +2,51 @@ from app.extensions import influxdb_write, mail
 from flask import current_app
 from flask_mail import Message
 from config import Config
+import json
 
 def notify(anomaly_detector, check, response):
-    current_app.logger.info(f"running anomaly_detector {anomaly_detector.id} of type {anomaly_detector.type} for check {check.id}")
+    log_dict = _create_log_dict(anomaly_detector, check)
     if anomaly_detector.type == "error":
-        _compare(response.status_code, anomaly_detector, check)
-    if anomaly_detector.type == "latency":
-        _compare(response.elapsed.microseconds / 1000, anomaly_detector, check)
+        _compare_status(log_dict, anomaly_detector, response)
+    elif anomaly_detector.type == "latency":
+        _compare_latency(log_dict, anomaly_detector, response)
+    
+def _compare_status(log_dict, anomaly_detector, response):
+    log_dict["comparitor"] = response.status_code
+    if int(anomaly_detector.value) >= response.status_code:
+        log_dict["detected"] = True
+        _send_notifications(anomaly_detector, log_dict)
+    else:
+        log_dict["detected"] = False
+    current_app.logger.info(log_dict)
 
-def _send_notifications(anomaly_detector):
+def _compare_latency(log_dict, anomaly_detector, response):
+    log_dict["comparitor"] = response.elapsed.microseconds / 1000
+    if response.elapsed.microseconds / 1000 >= int(anomaly_detector.value):
+         _send_notifications(anomaly_detector, log_dict)
+    else:
+        log_dict["detected"] = False
+    current_app.logger.info(log_dict)
+
+def _send_notifications(anomaly_detector, log_dict):
     if anomaly_detector.notification_channel is not None:
         channel = anomaly_detector.notification_channel
         current_app.logger.info(f"notifying {channel.name} {channel.type}")
         if channel.type == "email":
             subj = f"Anomaly {anomaly_detector.type} from Anomaly Detector {anomaly_detector.name}"
             message = Message(subj,
-                sender=Config.MAIL_DEFAULT_SENDER,
-                recipients=[channel.value])
+                              sender=Config.MAIL_DEFAULT_SENDER,
+                              body=json.dumps(log_dict),
+                              recipients=[channel.value])
             mail.send(message)
-            
-def _compare(comparitor, anomaly_detector, check):
-    if comparitor >= int(anomaly_detector.value):
-        current_app.logger.info(f"anomaly_detector {anomaly_detector.name} ({anomaly_detector.id}) conditions met for check {check.name} ({check.id})")
-        lp = f"anomalies,user_id={anomaly_detector.user.id},check={check.id},type={anomaly_detector.type} value={anomaly_detector.value}"
-        influxdb_write(lp)
-        _send_notifications(anomaly_detector)
-    else:
-        current_app.logger.info(f"anomaly_detector {anomaly_detector.name} ({anomaly_detector.id}) conditions not met for check {check.name} ({check.id})")
+
+def _create_log_dict(anomaly_detector, check):
+    log_dict = {"check_name": check.name,
+                "check_id": check.id,
+                "anomaly_detector_name": anomaly_detector.name,
+                "anomaly_detector_id": anomaly_detector.id,
+                "anomaly_detector_type": anomaly_detector.type,
+                "anomaly_detector_value": anomaly_detector.value
+                }
+
+    return log_dict
