3c0775b2f9cc40c3a26685f03eb8ca18958b0c97
Rick Spencer <rick@influxdata.com>
2022-12-31 12:18:45 -0800

    completing rename from notifications to anomaly_detectors

--- app/anomaly_detectors/routes.py
+++ app/anomaly_detectors/routes.py
@@ -3,6 +3,7 @@ from flask_user import login_required, current_user
 from app.anomaly_detectors import bp
 from app.extensions import db
 from app.models.anomaly_detectors import AnomalyDetector
+from app.models.checks import Check
 from app import iox_dbapi
 from config import Config
 
@@ -19,14 +20,14 @@ def index():
 def details(anomaly_detector_id):
     print(request.method)
     if request.method == "GET":
-        anomaly_detector = anomaly_detector.query.get(anomaly_detector_id)
+        anomaly_detector = AnomalyDetector.query.get(anomaly_detector_id)
         return render_template('anomaly_detectors/details.html', anomaly_detector=anomaly_detector)
 
 
 @bp.route('/<anomaly_detector_id>/delete', methods=["POST"])
 @login_required
 def delete(anomaly_detector_id):
-    anomaly_detector = anomaly_detector.query.get(anomaly_detector_id)
+    anomaly_detector = AnomalyDetector.query.get(anomaly_detector_id)
     if anomaly_detector.user_id == current_user.id:
         db.session.delete(anomaly_detector)
         db.session.commit()
@@ -36,11 +37,11 @@ def delete(anomaly_detector_id):
 @bp.route('<anomaly_detector_id>/edit', methods=["GET", "POST"])
 @login_required
 def edit(anomaly_detector_id):
-    anomaly_detector = anomaly_detector.query.get(anomaly_detector_id)
+    anomaly_detector = AnomalyDetector.query.get(anomaly_detector_id)
     if current_user.id is not anomaly_detector.user.id:
         return "", 404
     if request.method == "GET":
-        return render_template('/edit.html', anomaly_detector=anomaly_detector)
+        return render_template('anomaly_detectors/edit.html', anomaly_detector=anomaly_detector)
     elif request.method == "POST":
         anomaly_detector.name = request.form['name']
         anomaly_detector.value = request.form['value']
@@ -48,7 +49,6 @@ def edit(anomaly_detector_id):
         anomaly_detector.notification_channel_id = request.form['channel']
         db.session.commit()
         return redirect(url_for('anomaly_detectors.details', anomaly_detector_id=anomaly_detector_id))
-
 
 @bp.route('/issues')
 @login_required
@@ -61,10 +61,12 @@ def issues_table():
         token=Config.INFLUXDB_READ_TOKEN)
     cursor = connection.cursor()
     cursor.execute(sql)
-    t = "<TABLE><TR><TH>check id</TH><TH>type</TH><TH>value</TH><TH>time</TH><TR>"
+    t = "<TABLE><TR><TH>check name</TH><TH>check id</TH><TH>type</TH><TH>value</TH><TH>time</TH><TR>"
     d = cursor.fetchone()
+
     while d is not None:
-        t += f"<TR><TD>{d[2]}</TD><TD>{d[3]}</TD><TD>{d[4]}</TD><TD>{d[5]}</TD></TR>"
+        check = Check.query.get(d[2])
+        t += f"<TR><TD>{check.name}</TD><TD>{d[2]}</TD><TD>{d[3]}</TD><TD>{d[4]}</TD><TD>{d[5]}</TD></TR>"
         d = cursor.fetchone()
     t += "</TABLE>"
 
