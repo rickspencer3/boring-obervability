6b13e83e245ee598312d2b0a3b0e7781712f3ae3
Rick Spencer <rick@influxdata.com>
2023-02-26 16:21:38 -0500

    added database migration

--- app/models/notification_channels.py
+++ app/models/notification_channels.py
@@ -7,4 +7,5 @@ class NotificationChannel(db.Model):
     name = db.Column(db.String(100))
     value = db.Column(db.String(100))
     type = db.Column(db.String(20))
+    enabled = db.Column(db.Boolean, default=True)
     user = db.relationship("User", back_populates = "notification_channels")
