e653fcff4f110573cd0da1cf4e6719b05b0bd485
Rick Spencer <rick@influxdata.com>
2023-03-09 07:56:23 -0500

    added create header to headers index

--- app/headers/routes.py
+++ app/headers/routes.py
@@ -1,16 +1,24 @@
 from app.headers import bp
+from app.extensions import db
+from app.models.headers import Header
 from flask_user import login_required, current_user
 from flask import render_template, request, redirect, url_for
 
 
 @bp.route('/new', methods=["GET","POST"])
 @login_required
-def new(id):
+def new(id=None):
     if request.method == "GET":
         return render_template('headers/new.html', id=id)
 
     if request.method == "POST":
-        return redirect(url_for('checks.index'))
+        header = Header(name = request.form['name'],
+                key = request.form['key'], 
+                value = request.form['value'])
+        header.user = current_user
+        db.session.add(header)
+        db.session.commit()
+        return redirect(url_for('headers.index'))
 
 @bp.route('/', methods=["GET","POST"])
 @login_required
