58bb97dd65277d9209488e3eaca34a427ac733aa
Rick Spencer <rick@influxdata.com>
2023-04-02 12:49:52 -0400

    adding logging anomalies to influx. app is running, recording checks

--- app/models/anomaly_detectors.py
+++ app/models/anomaly_detectors.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.declarative import declared_attr
 from app.extensions import db
 from app.models.anomaly_detector_check import anomaly_detector_check_table
 from app.models.anomaly_detector_notification_channel import anomaly_detector_notification_channel_table
-
+from app.extensions import influxdb_write
 
 class AnomalyDetector(db.Model):
     __tablename__ = 'anomaly_detectors'
@@ -22,28 +22,38 @@ class AnomalyDetector(db.Model):
     def detect(self, check=None, response=None):
         pass
 
-    def _create_log_dict(self, check):
+    def _create_log_dict(self, check, response):
         log_dict = {"check_name": check.name,
                     "check_id": check.id,
                     "anomaly_detector_name": self.name,
                     "anomaly_detector_id": self.id,
-                    "anomaly_detector_type": self.type
+                    "anomaly_detector_type": self.type,
+                    "response_status": response.status_code,
+                    "response_latency": response.elapsed.microseconds / 1000
                     }
 
         return log_dict
-
 
 class LatencyDetector(AnomalyDetector):
     latency_lower_bound = db.Column(db.Integer)
 
     def detect(self, check=None, response=None):
-        log_dict = self._create_log_dict(check)
+        log_dict = self._create_log_dict(check, response)
+        latency = response.elapsed.microseconds / 1000
+        detected = latency > self.latency_lower_bound
+        log_dict['detected'] = detected
+
+        if detected:
+            lp = f"anomalies,check={check.id},type={self.type},user_id={self.user_id},id={self.id} latency_bound={self.latency_lower_bound},observed={latency}"
+            influxdb_write(lp)
+
         db.app.logger.info(log_dict)
 
-    def _create_log_dict(self, check):
-        d = super()._create_log_dict(check)
+    def _create_log_dict(self, check, response):
+        d = super()._create_log_dict(check, response)
         d["status_lower_bound"] = self.latency_lower_bound
         return d
+
     __mapper_args__ = {
         'polymorphic_identity': 'latency',
     }
@@ -52,11 +62,18 @@ class ErrorDetector(AnomalyDetector):
     status_lower_bound = db.Column(db.Integer)
 
     def detect(self, check=None, response=None):
-        log_dict = self._create_log_dict(check)
+        log_dict = self._create_log_dict(check, response)
+        detected = response.status_code > self.status_lower_bound
+        log_dict['detected'] = detected
+
+        if detected:
+            lp = f"anomalies,check={check.id},type={self.type},user_id={self.user_id},id={self.id} status_bound={self.status_lower_bound},observed={response.status_code}"
+            influxdb_write(lp)
+
         db.app.logger.info(log_dict)        
 
-    def _create_log_dict(self, check):
-        d = super()._create_log_dict(check)
+    def _create_log_dict(self, check, response):
+        d = super()._create_log_dict(check, response)
         d["status_lower_bound"] = self.status_lower_bound
         return d
 
